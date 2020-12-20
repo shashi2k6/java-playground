13f5c871b7f0026958311a4afd085194acbf84da
shashi <shashi2k6@gmail.com>
2020-12-20 13:51:16 -0500

    Final commit

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,6 +2,13 @@ package com.galvanize;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+        if(args.length<1){
+            System.out.println("Please provide an argument");
+        }else{
+            System.out.println(String.format("%s <%s>",args[0],args[1]));
+
+        }
+
+
     }
 }
